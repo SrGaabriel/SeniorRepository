e470b533f4244d6a0b37684af0a9c6a08240f5d5
WizardedBR <luizotavio4102@hotmail.com>
2020-05-30 20:45:02 -0300

    WallProvider was updated.

--- raid-plugin/src/main/java/com/raidplugin/sdk/provider/wall/WallProvider.java
+++ raid-plugin/src/main/java/com/raidplugin/sdk/provider/wall/WallProvider.java
@@ -1,25 +1,28 @@
 package com.raidplugin.sdk.provider.wall;
 
 import com.raidplugin.api.prototype.wall.Wall;
-import com.raidplugin.sdk.manager.WallManager;
+import com.raidplugin.sdk.inventory.item.ItemBuilder;
 import com.raidplugin.sdk.provider.nbt.NBTProvider;
-import com.raidplugin.sdk.repository.member.MemberRepository;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 public class WallProvider {
 
     private final NBTProvider nbtProvider = new NBTProvider();
-    private final MemberRepository memberRepository = MemberRepository.getInstance();
-    private final WallManager wallManager = WallManager.getInstance();
-
-    public void hasDamaged(Block block, Player breaker, int damage) {
-        // Wall event to damage.
-    }
 
     public ItemStack toItemStack(Wall wall) {
-        return null;
+        ItemStack itemStack = new ItemBuilder(Material.OBSIDIAN)
+                .name("§aWall Block")
+                .lore(
+                        " ",
+                        " §7That's a block to protect your team.",
+                        " §7Place block on your plot and see!",
+                        " "
+                ).build();
+
+        return nbtProvider.applyNBT(itemStack, nbt -> {
+            nbt.setInt("life", wall.getLife()); nbt.setInt("level", wall.getLevel());
+        });
     }
 
 }
